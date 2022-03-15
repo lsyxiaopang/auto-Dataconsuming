ac9bb9d022f778382aaefdf72fb0489af6d507c2
lsyxiaopang <lisongyuandy@foxmail.com>
2022-03-15 23:01:50 +0800

    修改了安装程序的一些错误

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ URL = 'https://github.com/lsyxiaopang/auto-Dataconsuming'
 EMAIL = 'lisongyuandy@foxmail.com'
 AUTHOR = 'Lisongyu'
 REQUIRES_PYTHON = '>=3.0.0'
-VERSION = '0.0.4'
+VERSION = '0.0.5'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
